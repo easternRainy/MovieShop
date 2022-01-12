5961176bce29f61e1a15fed72fb56d4ae209ac32
zhousicheng <github01.vacuolar@simplelogin.fr>
2022-01-11 18:08:16 -0800

    Add Entities: Cast, Crew, MovieCast, Role, User, UserRole, Review, Purchase, Favorite, MovieCrew

---
+++ ApplicationCore/Entities/Review.cs
@@ -0,0 +1,10 @@
+namespace ApplicationCore.Entities;
+
+public class Review
+{
+    public int MovieId;
+    public int UserId;
+    public decimal Rating;
+    public string? ReviewText;
+
+}
--- Intrastructure/Data/MovieShopDbContext.cs
+++ Intrastructure/Data/MovieShopDbContext.cs
@@ -11,23 +11,62 @@ public class MovieShopDbContext: DbContext
         
     }
     
+    public DbSet<Crew> Casts { get; set; }
+    public DbSet<Crew> Crews { get; set; }
+    public DbSet<Favorite> Favorites { get; set; } 
     public DbSet<Genre> Genres { get; set; }
-    
     public DbSet<Movie> Movies { get; set; }
-    
-    public DbSet<Trailer> Trailers { get; set; }
-    
+    public DbSet<MovieCast> MovieCasts { get; set; }
+    public DbSet<MovieCrew> MovieCrews { get; set; }
     public DbSet<MovieGenre> MovieGenres { get; set; }
+    public DbSet<Purchase> Purchases { get; set; }
+    public DbSet<Review> Reviews { get; set; }
+    public DbSet<Role> Roles { get; set; }
+    public DbSet<Trailer> Trailers { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<UserRole> UserRoles { get; set; }
+    
 
     // Fluent API to add constrains to database
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<Cast>(ConfigureCast);
+        modelBuilder.Entity<Crew>(ConfigureCrew); 
+        modelBuilder.Entity<Favorite>(ConfigureFavorite);
         modelBuilder.Entity<Movie>(ConfigureMovie);
-        modelBuilder.Entity<Trailer>(ConfigureTrailer);
+        modelBuilder.Entity<MovieCast>(ConfigureMovieCast);
+        modelBuilder.Entity<MovieCrew>(ConfigureMovieCrew);
         modelBuilder.Entity<MovieGenre>(ConfigureMovieGenre);
-
+        modelBuilder.Entity<Purchase>(ConfigurePurchase);
+        modelBuilder.Entity<Review>(ConfigureReview);
+        modelBuilder.Entity<Role>(ConfigureRole);
+        modelBuilder.Entity<Trailer>(ConfigureTrailer);
+        modelBuilder.Entity<User>(ConfigureUser);
+        modelBuilder.Entity<UserRole>(ConfigureUserRole);
     }
-    //
+    private void ConfigureCast(EntityTypeBuilder<Cast> builder)
+    {
+        builder.ToTable("Cast");
+        builder.HasKey(c => c.Id);
+        builder.Property(c => c.Name).HasMaxLength(128);
+        builder.Property(c => c.Gender).HasMaxLength(2084);
+        builder.Property(c => c.TmdbUrl).HasMaxLength(2084);
+        builder.Property(c => c.ProfilePath).HasMaxLength(2084);
+    }
+    private void ConfigureCrew(EntityTypeBuilder<Crew> builder)
+    {
+        builder.ToTable("Crew");
+        builder.HasKey(c => c.Id);
+        builder.Property(c => c.Name).HasMaxLength(128);
+        builder.Property(c => c.Gender).HasMaxLength(2084);
+        builder.Property(c => c.TmdbUrl).HasMaxLength(2084);
+        builder.Property(c => c.ProfilePath).HasMaxLength(2084);
+    }
+    private void ConfigureFavorite(EntityTypeBuilder<Favorite> builder)
+    {
+        builder.ToTable("Favorite");
+        builder.HasKey(c => c.Id);
+    }
     private void ConfigureMovie(EntityTypeBuilder<Movie> builder)
     {
         // here we are going to ue Fluent API to configure Movie Table
@@ -47,17 +86,19 @@ public class MovieShopDbContext: DbContext
         builder.Property(m => m.CreatedDate).HasDefaultValueSql("getdate()");
 
     }
-    
-    
-    private void ConfigureTrailer(EntityTypeBuilder<Trailer> builder)
+    private void ConfigureMovieCast(EntityTypeBuilder<MovieCast> builder)
     {
-        builder.ToTable("Trailer");
-        builder.HasKey(t => t.Id);
-        builder.Property(t => t.TrailerUrl).HasMaxLength(2084);
-        builder.Property(t => t.Name).HasMaxLength(2084);
-
+        builder.ToTable("MovieCast");
+        builder.HasKey(mc => new { mc.MovieId, mc.CastId });
+        builder.Property(mc => mc.Character).HasMaxLength(450);
     }
-
+    private void ConfigureMovieCrew(EntityTypeBuilder<MovieCrew> builder)
+    {
+        builder.ToTable("MovieCrew");
+        builder.HasKey(mc => new {mc.MovieId, mc.CrewId, mc.Department, mc.Job});
+        builder.Property(mc => mc.Department).HasMaxLength(128);
+        builder.Property(mc => mc.Job).HasMaxLength(128);
+    }
     private void ConfigureMovieGenre(EntityTypeBuilder<MovieGenre> builder)
     {
         builder.ToTable("MovieGenre");
@@ -66,6 +107,50 @@ public class MovieShopDbContext: DbContext
         builder.HasOne(m => m.Genre).WithMany(m => m.MoviesOfGenre).HasForeignKey(m => m.GenreId);
 
     }
+    private void ConfigurePurchase(EntityTypeBuilder<Purchase> builder)
+    {
+        builder.ToTable("Purchase");
+        builder.Property(p => p.TotalPrice).HasColumnType("decimal(18,2)");
+    }
+    private void ConfigureReview(EntityTypeBuilder<Review> builder)
+    {
+        builder.ToTable("Review");
+        builder.HasKey(r => new {r.MovieId, r.UserId});
+        builder.Property(r => r.Rating).HasColumnType("decimal(3,2)");
+        builder.Property(r => r.ReviewText).HasMaxLength(2084);
+    }
+    private void ConfigureRole(EntityTypeBuilder<Role> builder)
+    {
+        builder.ToTable("Role");
+        builder.HasKey(r => r.Id);
+        builder.Property(r => r.Name).HasMaxLength(20);
+    }
+    private void ConfigureTrailer(EntityTypeBuilder<Trailer> builder)
+    {
+        builder.ToTable("Trailer");
+        builder.HasKey(t => t.Id);
+        builder.Property(t => t.TrailerUrl).HasMaxLength(2084);
+        builder.Property(t => t.Name).HasMaxLength(2084);
+
+    }
+    private void ConfigureUser(EntityTypeBuilder<User> builder)
+    {
+        builder.ToTable("User");
+        builder.HasKey(u => u.Id);
+        builder.Property(u => u.FirstName).HasMaxLength(128);
+        builder.Property(u => u.LastName).HasMaxLength(128);
+        builder.Property(u => u.Email).HasMaxLength(256);
+        builder.Property(u => u.HashedPassword).HasMaxLength(1024);
+        builder.Property(u => u.Salt).HasMaxLength(1024);
+        builder.Property(u => u.PhoneNumber).HasMaxLength(16);
+    }
+    private void ConfigureUserRole(EntityTypeBuilder<UserRole> builder)
+    {
+        builder.ToTable("UserRole");
+        builder.HasKey(ur => new { ur.UserId, ur.RoleId });
+    }
+    
+    
 }
 
 
