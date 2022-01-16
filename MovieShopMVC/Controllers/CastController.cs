942a7c217ef90fd5d187ed3f31db81fc992b7c94
zhousicheng <github01.vacuolar@simplelogin.fr>
2022-01-15 17:27:08 -0800

    Add a placeholder for CastDetails display

--- MovieShopMVC/Controllers/CastController.cs
+++ MovieShopMVC/Controllers/CastController.cs
@@ -1,12 +1,26 @@
+using ApplicationCore.Contracts.Servies;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MovieShopMVC.Controllers;
 
 public class CastController : Controller
 {
+
+    private readonly ICastService _castService;
     // GET
     // public IActionResult Index()
     // {
     //     return View();
     // }
+
+    public CastController(ICastService castService)
+    {
+        _castService = castService;
+    }
+    
+    public async Task<IActionResult> Details(int id)
+    {
+        var castDetails = await _castService.GetCastDetails(id);
+        return View(castDetails);
+    }
 }
